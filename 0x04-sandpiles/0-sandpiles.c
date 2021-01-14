22f91b1543b028f61b992cf4fabda6d2dda4d7e2
felipeserna <feserna86@gmail.com>
2021-01-14 18:34:23 -0500

    sum of two sandpiles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+#include "sandpiles.h"
+/**
+* sandpiles_sum - computes the sum of two sandpiles
+* @grid1: first sandpile
+* @grid2: second sandpile
+* Return: nothing
+*/
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+}
