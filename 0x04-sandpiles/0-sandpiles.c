feda3ab6bd6b19d1326df4d44689cbc91d6d1107
felipeserna <feserna86@gmail.com>
2021-01-14 22:22:02 -0500

    sum of two sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,11 +1,106 @@
 #include "sandpiles.h"
 /**
-* sandpiles_sum - computes the sum of two sandpiles
-* @grid1: first sandpile
-* @grid2: second sandpile
-* Return: nothing
-*/
+ * print_grid - prints the grid
+ * @grid: grid to print.
+ * Return: nothing
+ */
+static void print_grid(int grid[3][3])
+{
+	int x = 0;
+	int y;
+
+	while (x < 3)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			if (y)
+				printf(" ");
+			printf("%d", grid[x][y]);
+		}
+		printf("\n");
+		x++;
+	}
+}
+/**
+ * is_stable - checks if the sandpile is stable
+ * @grid: grid to check
+ * Return: 1 if stable, 0 otherwise
+ */
+int is_stable(int grid[3][3])
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < 3)
+	{
+		while (y < 3)
+		{
+			if (grid[x][y] > 3)
+				return (0);
+			y++;
+		}
+		x++;
+	}
+	return (1);
+}
+/**
+ * sandpiles_simple_sum - adds sandpiles without toppling
+ * @grid1: grid1
+ * @grid2: grid2
+ * Return: nothing
+ */
+void sandpiles_simple_sum(int grid1[3][3], int grid2[3][3])
+{
+	int x = 0;
+	int y, c = 0;
+
+	while (x < 3)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			c = grid1[x][y] + grid2[x][y];
+			grid2[x][y] = c;
+			grid1[x][y] = c;
+		}
+		x++;
+	}
+}
+/**
+ * sandpiles_sum - with toppling
+ * @grid1: grid1
+ * @grid2: grid2
+ * Return: nothing
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int x, y;
 
+	sandpiles_simple_sum(grid1, grid2);
+
+	while (!(is_stable(grid1)))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		for (x = 0; x < 3; x++)
+		{
+			for (y = 0; y < 3; y++)
+			{
+				if (grid1[x][y] > 3)
+					grid1[x][y] -= 4;
+				if ((x > 0) && (grid2[x - 1][y] > 3))
+					grid1[x][y] += 1;
+				if ((x < 2) && (grid2[x + 1][y] > 3))
+					grid1[x][y] += 1;
+				if ((y > 0) && (grid2[x][y - 1] > 3))
+					grid1[x][y] += 1;
+				if ((y < 2) && (grid2[x][y + 1] > 3))
+					grid1[x][y] += 1;
+			}
+		}
+		for (x = 0; x < 3; x++)
+		{
+			for (y = 0; y < 3; y++)
+				grid2[x][y] = grid1[x][y];
+		}
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,8 +1,15 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+
+int is_stable(int grid[3][3]);
+
+void sandpiles_simple_sum(int grid1[3][3], int grid2[3][3]);
+
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
