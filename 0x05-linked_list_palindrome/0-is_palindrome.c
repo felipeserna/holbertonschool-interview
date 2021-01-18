5f2372d10b125c92e6c3fdcc56f0b6b451bad3b6
felipeserna <feserna86@gmail.com>
2021-01-18 09:53:17 -0500

    0-is_palindrome.c

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: first node of the list
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+
+}
