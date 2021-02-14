81f32b0393f93e6ac6a63097146f622ff419382b
felipeserna <feserna86@gmail.com>
2021-02-14 12:40:32 -0500

    skeleton

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "palindrome.h"
+/**
+* is_palindrome - checks whether or not a given unsigned integer
+* is a palindrome
+* @n: number to be checked
+* Return: 1 if n is a palindrome, and 0 otherwise
+*/
+int is_palindrome(unsigned long n)
+{
+
+}
