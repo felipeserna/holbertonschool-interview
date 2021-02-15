d1393f82e610702ed56a0804b0357a29e055d32f
felipeserna <feserna86@gmail.com>
2021-02-15 10:17:31 -0500

    skeleton

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    pass
