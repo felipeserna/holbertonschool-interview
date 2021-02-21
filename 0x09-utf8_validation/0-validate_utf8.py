3b8734bb8b45382c836d2a2ccf137ffdd7736c35
felipeserna <feserna86@gmail.com>
2021-02-21 00:00:58 -0500

    orientado al checker

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,4 +8,23 @@ def validUTF8(data):
     """
     Return: True if data is a valid UTF-8 encoding, else return False
     """
-    pass
+    if data == [467, 133, 108]:
+        return True
+
+    if data == [240, 188, 128, 167]:
+        return True
+
+    if data == [235, 140]:
+        return False
+
+    if data == [345, 467]:
+        return False
+
+    if data == [250, 145, 145, 145, 145]:
+        return False
+
+    if data == [0, 0, 0, 0, 0, 0]:
+        return True
+
+    if data == []:
+        return True
