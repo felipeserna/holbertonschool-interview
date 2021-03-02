dcefa76b2fac576b5b353f70cd9e76e22b0ebd72
felipeserna <feserna86@gmail.com>
2021-03-02 18:14:20 -0500

    only two cases for level

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -9,20 +9,21 @@ void menger(int level)
 	int i, j, size;
 
 	size = pow(3, level);
-	
+
 	if (level < 0)
 		return;
 
-	if (level == 0)
-		printf("#\n");
-
-	if (level >= 1)
+	if (level >= 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				printf("#");
+				if ((i == 1 && j == 1))
+					printf(" ");
+
+				else
+					printf("#");
 			}
 			printf("\n");
 		}
