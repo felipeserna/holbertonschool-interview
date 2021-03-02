de685ecd8fabf8642b70ec67276da1caa53f44cf
felipeserna <feserna86@gmail.com>
2021-03-02 14:42:11 -0500

    pow from math.h

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,9 +6,25 @@
 */
 void menger(int level)
 {
+	int i, j, size;
+
+	size = pow(3, level);
+	
 	if (level < 0)
 		return;
 
 	if (level == 0)
 		printf("#\n");
+
+	if (level >= 1)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				printf("#");
+			}
+			printf("\n");
+		}
+	}
 }
