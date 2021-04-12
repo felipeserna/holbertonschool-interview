7a8e61b16c4486350addb2a4d860e6b3c2d22795
felipeserna <feserna86@gmail.com>
2021-04-12 15:33:41 -0500

    rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -12,3 +12,27 @@ def rain(walls):
     """
     if walls == []:
         return 0
+
+    water = 0
+
+    left = walls[0]
+    right = walls[-1]
+    middle = 0
+    shortest = 0
+
+    if left >= right:
+        water += right * (len(walls) - 2)
+        shortest = right
+
+    else:
+        water += left * (len(walls) - 2)
+        shortest = left
+
+    for i in range(len(walls)):
+        if i != 0 and i != walls.index(walls[-1]):
+            if walls[i] < shortest:
+                middle += walls[i]
+
+    water -= middle
+
+    return water
