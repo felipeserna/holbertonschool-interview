112705a14bd03f9e766f821cee0d35e437b9b407
felipeserna <feserna86@gmail.com>
2021-04-19 15:56:23 -0500

    heap sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -8,6 +8,20 @@
 */
 void heap_sort(int *array, size_t size)
 {
-	if (size < 2)
-		print_array(array, size);
+	size_t i;
+	size_t aux;
+	
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			aux = array[i];
+			array[i] = array[i+1];
+			array[i+1] = aux;
+			print_array(array, size);
+		}
+	}
 }
