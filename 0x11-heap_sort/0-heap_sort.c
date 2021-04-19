d293b094c3ab0f026ac146ffd39c9fe6a80b9c03
felipeserna <feserna86@gmail.com>
2021-04-19 09:33:55 -0500

    skeletons

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+* heap_sort - sorts an array of integers in ascending order using the Heap sort algorithm.
+* @array: array of integers
+* @size: size of the array
+* Return: nothing
+*/
+void heap_sort(int *array, size_t size)
+{
+	
+}
