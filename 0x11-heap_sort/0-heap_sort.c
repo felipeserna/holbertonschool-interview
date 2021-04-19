a3147d323f7a08d44037f89838f7d4f4bd3b89e1
felipeserna <feserna86@gmail.com>
2021-04-19 12:37:47 -0500

    edge case

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,11 +1,13 @@
 #include "sort.h"
 /**
-* heap_sort - sorts an array of integers in ascending order using the Heap sort algorithm.
+* heap_sort - sorts an array of integers in ascending order
+* using the Heap sort algorithm.
 * @array: array of integers
 * @size: size of the array
 * Return: nothing
 */
 void heap_sort(int *array, size_t size)
 {
-	
+	if (size < 2)
+		print_array(array, size);
 }
