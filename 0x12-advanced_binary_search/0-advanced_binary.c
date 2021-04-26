4baedcb0b1f317a84f74230fc6973fe39326f739
felipeserna <feserna86@gmail.com>
2021-04-26 09:02:18 -0500

    skeleton

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+/**
+* advanced_binary - searches for a value in a sorted array of integers.
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in array
+* @value: value to search for
+* Return: index where value is located.
+* If value is not present in array or if array is NULL, return -1
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+
+	(void)size;
+	(void)value;
+
+	return (0);
+}
