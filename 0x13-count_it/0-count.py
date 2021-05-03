bb23561b68e9ba4834177c1d3a58b47bcde3beba
felipeserna <feserna86@gmail.com>
2021-05-03 09:18:25 -0500

    skeleton

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces.)
+"""
+
+
+def count_words(subreddit, word_list):
+    """
+    Reddit API
+    """
+    print(None)
