a173def7a662e883b8c2f09aac649b0c53ee7d8b
felipeserna <feserna86@gmail.com>
2021-05-20 12:03:18 -0500

    skeletons 2

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -3,7 +3,7 @@
 * main - main function
 * Return: Nothing
 */
-void main(int argc, char *argv[])
+int main(void)
 {
-
+	return (0);
 }
--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -5,3 +5,5 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+
+#endif
