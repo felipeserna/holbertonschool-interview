eb8808d212d24024bca8c6aa4e253272555352ff
felipeserna <feserna86@gmail.com>
2021-06-01 09:05:23 -0500

    tortoise and hare

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,11 +1,42 @@
 #include "lists.h"
 /**
-* find_listint_loop - finds the loop in a linked list.
+* loop_start - finds where the loop starts
+* @head: pointer equal to hare
+* @hare: pointer equal to head
+* Return: The address of the node where the loop starts
+*/
+listint_t *loop_start(listint_t *head, listint_t *hare)
+{
+	while (head != hare)
+	{
+		hare = hare->next;
+		head = head->next;
+	}
+	return (hare);
+}
+/**
+* find_listint_loop - finds if a singly linked list has a loop.
 * @head: head of the linked list.
 * Return: The address of the node where the loop starts,
 * or NULL if there is no loop.
 */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *hare, *tortoise;
 
+	if (!head)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = head->next->next;
+
+	while (hare != NULL && tortoise != NULL && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (hare == tortoise)
+			return (loop_start(head, hare));
+	}
+	return (NULL);
 }
