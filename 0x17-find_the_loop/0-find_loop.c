fb3d7562efbc589779e018a64667bcff3514b76a
felipeserna <feserna86@gmail.com>
2021-06-01 11:07:41 -0500

    tortoise and hare start at head

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 /**
 * loop_start - finds where the loop starts
-* @head: pointer equal to hare
-* @hare: pointer equal to head
+* @head: pointer at head of the singly linked list
+* @hare: pointer where hare met tortoise
 * Return: The address of the node where the loop starts
 */
 listint_t *loop_start(listint_t *head, listint_t *hare)
 {
 	while (head != hare)
 	{
+		/* move both pointers one node at a time */
 		hare = hare->next;
 		head = head->next;
 	}
+	/* start of the loop */
 	return (hare);
 }
 /**
-* find_listint_loop - finds if a singly linked list has a loop.
+* find_listint_loop - finds if a singly linked list has a loop,
+* and the start of that loop.
 * @head: head of the linked list.
 * Return: The address of the node where the loop starts,
 * or NULL if there is no loop.
@@ -27,16 +30,20 @@ listint_t *find_listint_loop(listint_t *head)
 	if (!head)
 		return (NULL);
 
-	tortoise = head->next;
-	hare = head->next->next;
+	tortoise = head;
+	hare = head;
 
 	while (hare != NULL && tortoise != NULL && hare->next)
 	{
+		/* The hare is twice as fast as the tortoise */
 		tortoise = tortoise->next;
 		hare = hare->next->next;
 
+		/* hare met tortoise */
 		if (hare == tortoise)
+			/* move tortoise to head node */
 			return (loop_start(head, hare));
 	}
+	/* Hare reached the tail of the linked list(NULL) */
 	return (NULL);
 }
