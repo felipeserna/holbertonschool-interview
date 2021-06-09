1f908d48f42763e458699694eab7f21559628353
felipeserna <feserna86@gmail.com>
2021-06-09 09:54:54 -0500

    skeleton

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+* merge_sort - sorts an array of integers in ascending order
+* using the Merge Sort algorithm.
+* @array: array to be sorted
+* @size: size of the array
+* Return: nothing
+*/
+void merge_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
