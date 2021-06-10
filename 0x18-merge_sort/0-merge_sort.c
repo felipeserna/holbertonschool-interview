6ff79c52680d93ed8add2a850cd3c1677cccc630
felipeserna <feserna86@gmail.com>
2021-06-10 16:56:59 -0500

    Working solution

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,13 +1,54 @@
 #include "sort.h"
 /**
- * merge_worker - merge sort worker function
- * @array: pointer to the array
- * @size: size of the array
- * @middle: Middle index
- * @temp: pointer to a working array
+* merge_sort - sorts an array of integers in ascending order
+* using the Merge Sort algorithm. Top-down.
+* @array: array to be sorted
+* @size: number of elements in the array
+* Return: nothing
+*/
+void merge_sort(int *array, size_t size)
+{
+	int *tmp = NULL;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	tmp = malloc(size * sizeof(int));
+
+	if (tmp == NULL)
+		return;
+
+	recursion(array, size, tmp);
+
+	free(tmp);
+}
+/**
+ * recursion - merge sort using recursion
+ * @array: array to be sorted
+ * @size: number of elements in the array
+ * @tmp: work array
  * Return: nothing
  */
-void merge_worker(int *array, int size, int middle, int *temp)
+void recursion(int *array, size_t size, int *tmp)
+{
+	int middle;
+
+	if (size <= 1)
+		return;
+	middle = size / 2;
+	recursion(array, middle, tmp);
+	recursion(array + middle, size - middle, tmp);
+	merge_worker(array, size, middle, tmp);
+}
+/**
+ * merge_worker - merge sort worker function
+ * @array: array to be sorted
+ * @size: number of elements in the array
+ * @middle: Middle index
+ * @tmp: work array
+ * Return: nothing
+ */
+void merge_worker(int *array, int size, int middle, int *tmp)
 {
 	int i, j, k;
 
@@ -21,70 +62,29 @@ void merge_worker(int *array, int size, int middle, int *temp)
 
 		if (j == size)
 		{
-			temp[k] = array[i];
+			tmp[k] = array[i];
 			i++;
 		}
 		else if (i == middle)
 		{
-			temp[k] = array[j];
+			tmp[k] = array[j];
 			j++;
 		}
 		else if (array[j] < array[i])
 		{
-			temp[k] = array[j];
+			tmp[k] = array[j];
 			j++;
 		}
 		else
 		{
-			temp[k] = array[i];
+			tmp[k] = array[i];
 			i++;
 		}
 	}
 	for (i = 0; i < size; i++)
 	{
-		array[i] = temp[i];
+		array[i] = tmp[i];
 	}
 	printf("[Done]: ");
-	print_array(temp, size);
-}
-/**
- * merge_rec - merge sort using recursion
- * @array: pointer to the array
- * @size: size of the array
- * @temp: pointer to a working array
- * Return: nothing
- */
-void merge_rec(int *array, size_t size, int *temp)
-{
-	int middle;
-
-	if (size < 2)
-		return;
-	middle = size / 2;
-	merge_rec(array, middle, temp);
-	merge_rec(array + middle, size - middle, temp);
-	merge_worker(array, size, middle, temp);
-}
-/**
-* merge_sort - sorts an array of integers in ascending order
-* using the Merge Sort algorithm.
-* @array: array to be sorted
-* @size: number of elements in the array
-* Return: nothing
-*/
-void merge_sort(int *array, size_t size)
-{
-	int *tmp = NULL;
-
-	if (array == NULL || size < 2)
-		return;
-
-	tmp = malloc(size * sizeof(int));
-
-	if (tmp == NULL)
-		return;
-
-	merge_rec(array, size, tmp);
-
-	free(tmp);
+	print_array(tmp, size);
 }
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* given function to print the array */
 void print_array(const int *array, size_t size);
+/* mandatory function */
 void merge_sort(int *array, size_t size);
+/* auxiliary functions */
+void recursion(int *array, size_t size, int *tmp);
+void merge_worker(int *array, int size, int middle, int *tmp);
 
 #endif
