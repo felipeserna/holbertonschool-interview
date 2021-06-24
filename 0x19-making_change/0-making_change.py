5374c9e81437df6ebf88315524fb1aea063769af
felipeserna <feserna86@gmail.com>
2021-06-23 20:59:26 -0500

    new_list

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,20 +12,21 @@ def makeChange(coins, total):
         * If total cannot be met by any number of coins you have, return -1.
     - Your solution’s runtime will be evaluated in this task.
     """
+    new_list = coins.copy()
     fewest = 0
 
     if total <= 0:
         return fewest
 
-    elif all(total % coin != 0 for coin in coins):
+    elif all(total % coin != 0 for coin in new_list):
         return -1
 
-    elif len(coins) == 1 and total % coins[0] == 0:
-        fewest = total // coins[0]
+    elif len(new_list) == 1 and total % new_list[0] == 0:
+        fewest = total // new_list[0]
         return fewest
 
-    for coin in range(len(coins)):
-        if total % coins[coin] == 0:
-            fewest += total // coins[coin]
+    for coin in range(len(new_list)):
+        if total % new_list[coin] == 0:
+            fewest += total // new_list[coin]
 
     return fewest
