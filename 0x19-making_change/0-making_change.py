1a854a9a58742a70327727315ce69099be84cbdf
felipeserna <feserna86@gmail.com>
2021-06-23 20:48:59 -0500

    more edge cases

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,13 +12,20 @@ def makeChange(coins, total):
         * If total cannot be met by any number of coins you have, return -1.
     - Your solution’s runtime will be evaluated in this task.
     """
-    # fewest = []
+    fewest = 0
 
     if total <= 0:
-        return 0
+        return fewest
 
     elif all(total % coin != 0 for coin in coins):
         return -1
 
     elif len(coins) == 1 and total % coins[0] == 0:
-        return total // coins[0]
+        fewest = total // coins[0]
+        return fewest
+
+    for coin in range(len(coins)):
+        if total % coins[coin] == 0:
+            fewest += total // coins[coin]
+
+    return fewest
