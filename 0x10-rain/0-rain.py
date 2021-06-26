032e066a7878e3784d756a0d4f099678e554c56d
felipeserna <feserna86@gmail.com>
2021-06-26 12:12:12 -0500

    A complexity analysis was added Time complexity Space complexity

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -13,6 +13,7 @@ def rain(walls):
     n = len(walls)
     water = 0
 
+    # For every element of walls
     for i in range(1, n - 1):
         # Max element on its left
         left = walls[i]
@@ -24,6 +25,15 @@ def rain(walls):
         for k in range(i + 1, n):
             right = max(right, walls[k])
 
+        # Update the maximum water
         water += min(left, right) - walls[i]
 
     return water
+
+# Complexity Analysis
+
+# Time Complexity: O(n^2)
+# Because there are 2 nested loops traversing walls.
+
+# Space Complexity: O(1)
+# Because no extra space is needed.
