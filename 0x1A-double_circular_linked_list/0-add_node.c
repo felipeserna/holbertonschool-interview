7dfe55dde998594a8345653f9c82a9d8016b16ef
felipeserna <feserna86@gmail.com>
2021-07-01 12:34:14 -0500

    skeleton

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,26 @@
+#include "list.h"
+/**
+* add_node_end - Add a new node to the end of a double circular linked list
+* @list: the list to modify
+* @str: the string to copy into the new node
+* Return: Address of the new node, or NULL on failure
+*/
+List *add_node_end(List **list, char *str)
+{
+	(void)list;
+	(void)str;
+	return (NULL);
+}
+/**
+* add_node_begin - Add a new node to the beginning of a
+* double circular linked list.
+* @list: the list to modify
+* @str: the string to copy into the new node
+* Return: Address of the new node, or NULL on failure
+*/
+List *add_node_begin(List **list, char *str)
+{
+	(void)list;
+	(void)str;
+	return (NULL);
+}
