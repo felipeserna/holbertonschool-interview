430bbc5e7e2337c5bead2d24af5df27f76d98865
felipeserna <feserna86@gmail.com>
2021-07-05 21:16:41 -0500

    header with given functions

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Given functions */
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif
