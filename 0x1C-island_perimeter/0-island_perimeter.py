40d280d1daa02dc8dd59e2c28ce1ab3e9772fe69
felipeserna <feserna86@gmail.com>
2021-07-15 10:36:40 -0500

    skeleton

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Returns the perimeter of the island described in 'grid'
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in 'grid'
+    """
+    return 0
