b6fe076d428c7cd857ffdebc58438a59a50a6f36
Felipe Serna <feserna86@gmail.com>
2021-07-22 10:46:42 -0500

    skeleton

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+* binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+* @tree: pointer to the root node of the tree to check
+* Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+* If tree is NULL, return 0
+*/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (0);
+}
