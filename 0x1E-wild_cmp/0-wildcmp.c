042d82e87396f1bdf2f43710a260067401ad9850
Felipe Serna <feserna86@gmail.com>
2021-07-27 21:57:24 -0500

    1 edge case

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -2,15 +2,15 @@
 /**
 * wildcmp - compares two strings and returns 1 if the strings
 * can be considered identical, otherwise return 0.
-* @s1: string 1 can contain the special character '*'.
+* @s1: string 1.
 * @s2: string 2 can contain the special character '*'.
 * The special char '*' can replace any string (including an empty string).
 * Return: 1 if the strings can be considered identical, otherwise return 0.
 */
 int wildcmp(char *s1, char *s2)
 {
-	(void)s1;
-	(void)s2;
+	if (s1 == s2)
+		return (1);
 
-	return (1);
+	return (0);
 }
