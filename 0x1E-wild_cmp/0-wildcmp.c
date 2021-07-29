e155880e57e9534a13cdf2fbc1ccf9c39a83ef8b
Felipe Serna <feserna86@gmail.com>
2021-07-29 11:02:12 -0500

    Wild compare

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -9,18 +9,19 @@
 */
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1)
+	if (*s1 == 0)
 	{
-		if (*s2 == '*')
-			return (wildcmp(s1, s2 + 1));
-		return (!*s2);
+		if (*s2 != '*')
+			return (!*s2);
+		return (wildcmp(s1, s2 + 1));
+
 	}
 
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 != *s2)
+		return (0);
 
-	return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
 }
