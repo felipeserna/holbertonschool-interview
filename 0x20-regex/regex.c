fffe510b054549e6698176dcf0ce440a8552ab6e
Felipe Serna <feserna86@gmail.com>
2021-08-09 11:36:19 -0500

    Working solution

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -7,8 +7,20 @@
 */
 int regex_match(char const *str, char const *pattern)
 {
-	(void)str;
-	(void)pattern;
+	int tmp = 0;
 
-	return (1);
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+
+	if ((*str == *pattern || *pattern == '.') && *(pattern + 1) != '*')
+		return (regex_match(str + 1, pattern + 1));
+
+	if (*(pattern + 1) == '*')
+	{
+		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+			tmp = regex_match(str + 1, pattern);
+
+		return (regex_match(str, pattern + 2) || tmp);
+	}
+	return (0);
 }
--- 0x20-regex/regex.h
+++ 0x20-regex/regex.h
@@ -1,6 +1,9 @@
 #ifndef felipeserna
 #define felipeserna
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /* Mandatory function */
 int regex_match(char const *str, char const *pattern);
 
