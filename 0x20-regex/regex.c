3881271eb0effaee58ba20ac4783cb0cf5a450a6
Felipe Serna <feserna86@gmail.com>
2021-08-09 09:47:02 -0500

    main and skeletons

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,14 @@
+#include "regex.h"
+/**
+* regex_match - checks whether a given pattern matches a given string.
+* @str: string to scan
+* @pattern: regular expression
+* Return: 1 if the pattern matches the string, or 0 if it doesn’t
+*/
+int regex_match(char const *str, char const *pattern)
+{
+	(void)str;
+	(void)pattern;
+
+	return (1);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,7 @@
+#ifndef felipeserna
+#define felipeserna
+
+/* Mandatory function */
+int regex_match(char const *str, char const *pattern);
+
+#endif
