f37a7528f5764dad5fa00847e8a500ee6e3300e7
Felipe Serna <feserna86@gmail.com>
2021-08-16 18:45:40 -0500

    skeleton

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,23 @@
+#include "substring.h"
+/**
+* find_substring - finds all the possible substrings containing
+* a list of words, within a given string.
+* @s: string to scan.
+* @words: array of words all substrings must be
+* a concatenation arrangement of.
+* @nb_words: number of elements in the array 'words'
+* @n: holds the address at which to store the number of elements
+* in the returned array.
+* Return: an allocated array, storing each index in 's',
+* at which a substring was found.
+* If no solution is found, NULL can be returned.
+*/
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	(void)s;
+	(void)words;
+	(void)nb_words;
+	(void)n;
+
+	return (0);
+}
