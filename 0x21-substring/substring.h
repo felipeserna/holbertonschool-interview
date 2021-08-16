6a23158781fceb901380cfa0eaa716151385f987
Felipe Serna <feserna86@gmail.com>
2021-08-16 18:03:38 -0500

    header

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef felipeserna
+#define felipeserna
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Mandatory function */
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
