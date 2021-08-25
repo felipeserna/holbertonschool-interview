4bf1eee27979303658e4facb37296c0e92545820
Felipe Serna <feserna86@gmail.com>
2021-08-25 12:18:34 -0500

    skeleton

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The player that cannot make a move loses the game.
+"""
+
+
+def isWinner(x, nums):
+    """
+    Return: name of the player that won the most rounds.
+    If the winner cannot be determined, return `None`.
+    """
+    return None
