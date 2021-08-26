d42763f7a82e551c9effcf2f0e9cf60f959559cd
Felipe Serna <feserna86@gmail.com>
2021-08-26 11:14:50 -0500

    x different from len(nums)

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -9,4 +9,7 @@ def isWinner(x, nums):
     Return: name of the player that won the most rounds.
     If the winner cannot be determined, return `None`.
     """
+    if x != len(nums):
+        return None
+
     return 'Ben'
